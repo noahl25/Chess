f5426b084f32a5bb17f3708df49131652338a2a0
noahl25 <97693037+noahl25@users.noreply.github.com>
2023-03-20 12:23:55 -0700

    Add files via upload

---
+++ Chess/Tile.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "MacrosIncludes.h";
+
+class Tile {
+
+private:
+	
+	Pieces piece;
+	Color color;
+
+public:
+
+	Tile(Pieces piece = Pieces::BLANK, Color color = Color::WHITE);
+	bool changeTile(Pieces piece);
+	Pieces getType();
+	std::string toString();
+	Color getColor();
+};
