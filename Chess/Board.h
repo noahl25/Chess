85e8f4744651a19e0ae8c1f4c0a936631930b8ac
noahl25 <97693037+noahl25@users.noreply.github.com>
2023-04-05 19:06:06 -0700

    Add files via upload

---
+++ Chess/Board.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "MacrosIncludes.h"
+
+class Tile;
+
+class Board {
+
+private:
+
+	std::vector< std::vector<Tile*> > boardTiles;
+	bool bWhiteMove = true;
+	bool validatePieceMove(int x, int y, int xTo, int yTo);
+	bool colorCheck(int x, int y, int xTo, int yTo);
+	
+	
+	struct PrevMove {
+
+		int fromX;
+		int fromY;
+
+		int toX;
+		int toY;
+
+		Tile* pieceTaken;
+
+		PrevMove(int x, int y, int tx, int ty, Tile* piece);
+		PrevMove();
+
+	} prevMove;
+
+
+	inline bool isBlank(int x, int y);
+
+public:
+
+	struct KingLoc {
+		int x;
+		int y;
+	} WhiteKing, BlackKing;
+
+	std::vector< std::vector<Tile*> > getBoard();
+	void translateXY(int& x, int& y);
+	Tile* getTileAtPoint(int x, int y);
+	void setTileAtPoint(int x, int y, Tile* tile);
+	Board::KingLoc getKingLocation(Color color);
+
+	void makeMove(str from, str to, bool underCheck);
+	Move validateMove(char x, int y, char xTo, int yTo);
+	void undo();
+
+	bool underAttack(Color opp, int x, int y);
+	bool underAttack(Color opp, int x, int y, int& xOut, int& yOut, bool isDefending = false);
+
+	void sayHi() { std::cout << "hi"; endl; }
+
+	void debug();
+
+	int boardWidth = 3;
+
+	bool getMove();
+	void setMove(bool move);
+
+	Board();
+
+	~Board() {
+		boardTiles.clear();
+	}
+	
+
+};
